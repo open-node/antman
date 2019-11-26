6e00363bdc2e714c42448b3abc4a3ee51e517efb
Redstone Zhao <zhaoxiongfei@nequal.com>
2019-11-26 16:35:32 +0800

    remove debugger info

--- src/cli/antman.js
+++ src/cli/antman.js
@@ -6,7 +6,6 @@ const actions = require("./actions/");
 const action = process.argv[2];
 const args = process.argv.slice(3);
 
-console.log(actions);
 const fn = actions[action];
 if (!util.isFunction(fn)) throw Error(`Not found action: ${action}`);
 fn(...args);
